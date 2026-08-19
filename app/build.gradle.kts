5ca43fba4e49bb0a0f43ea289913f85da077d273
ZYJ882 <1.09709327e+08+ZYJ882@users.noreply.github.com>
2026-08-19 17:01:39 +0000

    feat: use selected rounded infinite stream icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.streamlark.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
